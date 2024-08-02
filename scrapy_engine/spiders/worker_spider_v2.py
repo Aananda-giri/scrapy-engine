97fc4a04bc7d7a5a0472a41034f43801e03a1e4c
Aananda-Giri <60064717+Aananda-giri@users.noreply.github.com>
2024-08-02 09:24:45 +0545

    check if Content-Type is text/html before attempting to save paragraphs

--- scrapy_engine/spiders/worker_spider_v2.py
+++ scrapy_engine/spiders/worker_spider_v2.py
@@ -74,10 +74,20 @@ class MasterSlave(scrapy.Spider):
         for url in start_urls:
             yield scrapy.Request(url, callback=self.parse, errback=self.errback_httpbin)  # , dont_filter=True  : allows visiting same url again
     def parse(self, response):
+        if 'text/html' not in response.headers.get('Content-Type', '').decode('utf-8'):
+            # response is not text/html (its probably some document)
+            # save the link to mongo
+            self.mongo.db['other_data'].insert_one(
+                {
+                    'url': response.url,
+                    'Content-Type': response.headers.get('Content-Type', '').decode('utf-8'),
+                }
+            )
+            return
         links = LinkExtractor(deny_extensions=[]).extract_links(response)
         drive_links = []    # Drive links
         site_links = []     # website links
-  
+        
         # yield every heading, paragraph from current page
         headings_and_paragraphs = response.css('h1::text, h2::text, h3::text, h4::text, h5::text, h6::text, p::text').getall()
         if self.crawl_paragraph_data:
