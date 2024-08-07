961cea96df7aa346f4ba602ead3aab672282273a
Aananda-Giri <60064717+Aananda-giri@users.noreply.github.com>
2024-08-07 10:45:24 +0545

    commit befor crawled_data format change

--- scrapy_engine/spiders/test_spiders/error_catching_spider.py
+++ scrapy_engine/spiders/test_spiders/error_catching_spider.py
@@ -32,8 +32,8 @@ class MasterSlave(scrapy.Spider):
         
     def start_requests(self):
         start_urls = ['https://daodolpa.moha.gov.np/page/gallery'] # > (failed 3 times): 500 Internal Server Error
-        start_urls.append('https://daodolpa.moha.gov.np/page/gallery') # Status code: 500
         start_urls.append('https://daorupandehi.moha.gov.np/%E0%A4%88%E0%A4%B2%E0%A4%BE%E0%A4%95%E0%A4%BE%20%E0%A4%AA%E0%A5%8D%E0%A4%B0%E0%A4%B6%E0%A4%BE%E0%A4%B8%E0%A4%A8%20%E0%A4%95%E0%A4%BE%E0%A4%B0%E0%A5%8D%E0%A4%AF%E0%A4%BE%E0%A4%B2%E0%A4%AF%20%20%E0%A4%9A%E0%A4%82%E0%A4%96%E0%A5%87%E0%A4%B2%E0%A5%80%20%E0%A4%9C%E0%A4%BE%E0%A4%9C%E0%A4%B0%E0%A4%95%E0%A5%8B%E0%A4%9F') # 404
+        start_urls.append('https://shivrajmun.gov.np/en/content/%E0%A4%AA%E0%A5%82%E0%A4%B0%E0%A5%8D%E0%A4%A3%E0%A4%AC%E0%A4%B9%E0%A4%BE%E0%A4%A6%E0%A5%81%E0%A4%B0-%E0%A4%B5%E0%A4%BF%E0%A4%B6%E0%A5%8D%E2%80%8D%E0%A4%B5%E0%A4%95%E0%A4%B0%E0%A5%8D%E0%A4%AE%E0%A4%BE-0') # 403
         for url in start_urls:
             yield scrapy.Request(url, callback=self.parse, errback=self.errback_httpbin)  # , dont_filter=True  : allows visiting same url again
     def parse(self, response):
